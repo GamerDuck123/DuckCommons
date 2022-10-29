367e0325d70bcc1e4b3f63253c2f41f0efcedd07
BuildTools <gtpalm42@gmail.com>
2022-10-29 02:39:54 -0400

    Changed PlayerArmorChangeEvent.java to be an "enhancer" so it builds off of PlayerArmorChangeEvent inside of Paper instead of creating a new event... more enhancers and events to come soon

--- src/main/java/com/gamerduck/commons/events/extra/enhancers/EnhancePlayerArmorChangeEvent.java
+++ src/main/java/com/gamerduck/commons/events/extra/enhancers/EnhancePlayerArmorChangeEvent.java
@@ -1,63 +1,48 @@
-package com.gamerduck.commons.events.extra.handlers;
+package com.gamerduck.commons.events.extra.enhancers;
 
-import com.gamerduck.commons.events.extra.PlayerArmorChangeEvent;
-import com.gamerduck.commons.events.extra.PlayerHandChangeEvent;
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseArmorEvent;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerItemHeldEvent;
-import org.bukkit.event.player.PlayerSwapHandItemsEvent;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent.SlotType;
 
 import static org.bukkit.Material.AIR;
 
-public class PlayerArmorChangeEventHandler implements Listener {
+public class EnhancePlayerArmorChangeEvent implements Listener {
 
     public static void enable(JavaPlugin plugin) {
-        plugin.getServer().getPluginManager().registerEvents(new PlayerArmorChangeEventHandler(), plugin);
-    }
-
-    @EventHandler
-    public void armorChangeHandler(com.destroystokyo.paper.event.player.PlayerArmorChangeEvent e) {
-        Bukkit.getPluginManager().callEvent(new PlayerArmorChangeEvent(e.getPlayer(), e.getNewItem(), e.getOldItem(), e.getSlotType()));
+        plugin.getServer().getPluginManager().registerEvents(new EnhancePlayerArmorChangeEvent(), plugin);
     }
 
     @EventHandler
     public void dispenserHandler(BlockDispenseArmorEvent e) {
         if (e.getTargetEntity() instanceof Player p) {
             boolean shouldFire = false;
-            SlotType slotType = null;
+            PlayerArmorChangeEvent.SlotType slotType = null;
             if (e.getItem().getType().toString().contains("HELMET")
                     && (p.getInventory().getHelmet() == null || p.getInventory().getHelmet().getType() == AIR)) {
                 shouldFire = true;
-                slotType = SlotType.HEAD;
+                slotType = PlayerArmorChangeEvent.SlotType.HEAD;
             }
             if (e.getItem().getType().toString().contains("CHESTPLATE")
                     && (p.getInventory().getChestplate() == null || p.getInventory().getChestplate().getType() == AIR)) {
                 shouldFire = true;
-                slotType = SlotType.CHEST;
+                slotType = PlayerArmorChangeEvent.SlotType.CHEST;
             }
             if (e.getItem().getType().toString().contains("LEGGINGS")
                     && (p.getInventory().getLeggings() == null || p.getInventory().getLeggings().getType() == AIR)) {
                 shouldFire = true;
-                slotType = SlotType.LEGS;
+                slotType = PlayerArmorChangeEvent.SlotType.LEGS;
             }
             if (e.getItem().getType().toString().contains("BOOTS")
                     && (p.getInventory().getBoots() == null || p.getInventory().getBoots().getType() == AIR)) {
                 shouldFire = true;
-                slotType = SlotType.FEET;
+                slotType = PlayerArmorChangeEvent.SlotType.FEET;
             }
-            if (shouldFire) Bukkit.getPluginManager().callEvent(new PlayerArmorChangeEvent(p, e.getItem(), new ItemStack(AIR), slotType));
+            if (shouldFire) Bukkit.getPluginManager().callEvent(new PlayerArmorChangeEvent(p, slotType, new ItemStack(AIR), e.getItem()));
         }
     }
-
 }
