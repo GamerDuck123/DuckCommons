5df7277c4badb6ab3302e2353874052f6e933d7b
BuildTools <gtpalm42@gmail.com>
2022-10-29 01:27:55 -0400

    Fixed PlayerHandChangeEventHandler

--- src/main/java/com/gamerduck/commons/events/extra/handlers/PlayerHandChangeEventHandler.java
+++ src/main/java/com/gamerduck/commons/events/extra/handlers/PlayerHandChangeEventHandler.java
@@ -4,16 +4,22 @@ import com.gamerduck.commons.events.extra.PlayerHandChangeEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import static org.bukkit.Material.AIR;
 
-public class PlayerHandChangeEventHandler extends DuckEventHandler {
+public class PlayerHandChangeEventHandler implements Listener {
+
+    public static void enable(JavaPlugin plugin) {
+        plugin.getServer().getPluginManager().registerEvents(new PlayerHandChangeEventHandler(), plugin);
+    }
 
     @EventHandler
     public void handChangeHandler(PlayerItemHeldEvent e) {
